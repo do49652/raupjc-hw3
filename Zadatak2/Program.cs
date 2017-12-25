78a0f5f620be7c803e14c9765e97d4f8076c4ebf
Dario Oroz <dario.oroz@fer.hr>
2017-12-25 08:35:04 +0100

    added error logging.... (forgot)

--- Zadatak2/Program.cs
+++ Zadatak2/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Zadatak2
 {
@@ -20,6 +21,17 @@ namespace Zadatak2
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog((context, logger) =>
+                {
+                    var cnnstr = context.Configuration["DefaultConnection"];
+
+                    logger.MinimumLevel.Error()
+                        .Enrich.FromLogContext()
+                        .WriteTo.MSSqlServer(
+                            connectionString: cnnstr,
+                            tableName: "ErrorLogs",
+                            autoCreateSqlTable: true);
+                })
                 .Build();
     }
 }
